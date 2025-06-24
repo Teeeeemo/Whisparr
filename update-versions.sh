346b047200e9459d205dd8a0e76866e9720d6f90
Teeeeemo <Teeemo@hehe.nope>
2025-06-24 15:52:52 +0800

    .

---
+++ update-versions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sbranch="eros"
+version=$(curl -fsSL "https://whisparr.servarr.com/v1/update/${sbranch}/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -re '.[0].version') || exit 1
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version//v/}" \
+    --arg sbranch "${sbranch}" \
+    '.version = $version | .sbranch = $sbranch' <<< "${json}" | tee VERSION.json
